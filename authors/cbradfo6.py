aa42a0bb90d88baddd1614152e417f5f9d9d8218
Courtney Bradford <cbradfo6@bulldogs.aamu.edu>
2017-10-21 12:31:01 -0500

    Added cbradfo6 to author page.

--- authors/cbradfo6.py
+++ authors/cbradfo6.py
@@ -14,26 +14,16 @@
 
 import webapp2
 
-from authors import amason6
-from authors import cbradfo6
-from authors import jbeatty
-from authors import tlarsen
-from authors import tthomp37
+import author_pages
 
-from authors import author_pages
-
-
-class HomePage(webapp2.RequestHandler):
-    """The / home page of the "Hello, world!" app."""
+class AuthorPage(webapp2.RequestHandler):
+    """The cbradfo6 author page of the GiR App Labs at AAMU app."""
 
     def get(self):
-        """HTTP GET handler for the "Hello, world!" app."""
+        """HTTP GET handler for the tlarsen Users page."""
 
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write("What's up, It's Court!")
 
 
-routes = [('/', HomePage),]
-routes.extend(author_pages.routes())
-
-app = webapp2.WSGIApplication(routes, debug=True)
+author_pages.add_page('cbradfo6', AuthorPage)
