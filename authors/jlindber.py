b2a98f0d24c88d7cd1f02dd32036d05ac3bf0e3d
Justin Lindberg <jlindber@bulldogs.aamu.edu>
2017-10-21 12:31:31 -0500

    Fixed users -> authors; added last name.

--- authors/jlindber.py
+++ authors/jlindber.py
@@ -14,25 +14,16 @@
 
 import webapp2
 
-from authors import amason6
-from authors import jbeatty
-from authors import tlarsen
-from authors import tthomp37
-from authors import jlindber
-from authors import author_pages
+import author_pages
 
-
-class HomePage(webapp2.RequestHandler):
-    """The / home page of the "Hello, world!" app."""
+class AuthorPage(webapp2.RequestHandler):
+    """The jlindber author home page of the GiR App Labs at AAMU app."""
 
     def get(self):
-        """HTTP GET handler for the "Hello, world!" app."""
+        """HTTP GET handler for the tlarsen Users page."""
 
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('It's Justin')
+        self.response.write("Wassup, it's Justin!")
 
 
-routes = [('/', HomePage),]
-routes.extend(author_pages.routes())
-
-app = webapp2.WSGIApplication(routes, debug=True)
+author_pages.add_page('jlindber', AuthorPage)
